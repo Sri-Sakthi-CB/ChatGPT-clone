1c55610dd95a7a3082a862d0a3b8ca74b6f7a354
Sri-Sakthi-CB <srisakthi1509@gmail.com>
2024-07-09 01:07:19 +0530

    initial commit

---
+++ src/openai.js
@@ -0,0 +1,19 @@
+const { Configuration, OpenAIApi } = require('openai');
+
+const API_KEY = process.env.API_KEY;
+
+const configuration= new Configuration({ apiKey: API_KEY });
+const openai = new OpenAIApi(configuration);
+
+export async function sendMsgToOpenAI(message) {
+    const res = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: message,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0
+    });
+    return res.data.choices[0].text;
+}
